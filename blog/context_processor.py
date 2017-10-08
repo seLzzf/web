73a297848f3b9a92566b382250b4fa62adc5715f
zz <271938333@qq.com>
2017-10-08 23:21:00 +0800

    msg_del

--- blog/context_processor.py
+++ blog/context_processor.py
@@ -2,5 +2,8 @@ from users.models import Userinfo
 
 def request_userinfo(request):
 	user=request.user
-	request_userinfo=Userinfo.objects.get(user=user)
-	return {'request_userinfo':request_userinfo}
+	try:
+		request_userinfo=Userinfo.objects.get(user=user)
+		return {'request_userinfo':request_userinfo}
+	except:
+		return {'request_userinfo':'请登录'}
